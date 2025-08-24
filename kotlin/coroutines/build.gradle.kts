107beccd1364b7ab637e749a6cfe0b99eb32fbc2
Hasan Demirtaş <utsukushihito@outlook.com>
2025-08-24 19:08:34 +0300

    remove unused deps.

--- kotlin/coroutines/build.gradle.kts
+++ kotlin/coroutines/build.gradle.kts
@@ -6,14 +6,5 @@ applyPublish("kotlin-coroutines")
 
 dependencies {
     compileOnly(project(":common"))
-    compileOnly(project(":kotlin-extensions"))
-    compileOnly(kotlin("stdlib"))
     compileOnly(libs.kotlinx.coroutines)
-
-    testImplementation(project(":common"))
-    testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    testImplementation("org.mockito:mockito-core:5.5.0")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
 }
--- kotlin/extensions/build.gradle.kts
+++ kotlin/extensions/build.gradle.kts
@@ -6,11 +6,4 @@ applyPublish("kotlin")
 
 dependencies {
     compileOnly(project(":common"))
-    compileOnly(kotlin("stdlib"))
-
-    testImplementation(project(":common"))
-    testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    testImplementation("org.mockito:mockito-core:5.5.0")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
 }
